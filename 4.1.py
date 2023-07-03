4f41f7e4d190a3185a3eb6ee875cc457ff8e9ac0
arsss <m.curtsev@gmail.com>
2023-07-03 21:57:42 +0300

    initial

---
+++ 4.1.py
@@ -0,0 +1,21 @@
+ #O (N**2)
+# def strcount(s):
+#     for sym in set(s):
+#         counter = 0
+#         for sub_sym in s:
+#             if sym == sub_sym:
+#                 counter += 1
+#         print(f'{sym} - {counter}')
+
+
+# strcount('aapdin')
+# O (N)
+def strcounter(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(f'{sym} - {count}')
+    
+strcounter('aapdin')
